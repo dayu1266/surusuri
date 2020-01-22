f283df6f236c6d0c74ef3982932294eed22e3e4a
kobayashi <nyarlathotep3783@docomo.ne.jp>
2020-01-22 13:32:05 +0900

    タイトル画面の処理の追加（小林）

--- sumisumo/src/Scenes/TitleScene.cs
+++ sumisumo/src/Scenes/TitleScene.cs
@@ -10,23 +10,69 @@ namespace sumisumo
 {
     class TitleScene : Scene
     {
+        enum State
+        {
+            Scroll,
+            Ready,
+            Flash,
+        }
+        int logoPosY;
+        State state;
+        int flashTimer;
+        int flashInterval;
         public override void Init()
         {
+            state = State.Scroll;
+            logoPosY = -64;
+            flashTimer = 0;
+            flashInterval = 0;
         }
 
         public override void Update()
         {
-            if (Input.GetButtonDown(DX.PAD_INPUT_1))
+            if (state == State.Scroll)
             {
-                Game.ChangeScene(new PlayScene());
+                logoPosY += 1;
+                if (logoPosY >= 250)
+                {
+                    state = State.Ready;
+                }
+                if (Input.GetButtonDown(DX.PAD_INPUT_1))
+                {
+                    logoPosY = 250;
+                }
             }
-
+            if (state == State.Ready)
+            {
+                flashTimer++;
+                if (Input.GetButtonDown(DX.PAD_INPUT_1))
+                {
+                    state = State.Flash;
+                }
+            }
+            if (state == State.Flash)
+            {
+                flashTimer++;
+                flashInterval++;
+                if (flashInterval >= 200)
+                {
+                    Game.ChangeScene(new PlayScene());
+                }
+            }
         }
 
         public override void Draw()
         {
-            DX.DrawGraph(0, 0, Image.play_bg,1);
-            DX.DrawRotaGraph((int)Screen.Size.X/2, 250, 6, 0, Image.titlelogo, 1);
+            DX.DrawGraph(0, 0, Image.play_bg, 1);
+            DX.DrawRotaGraph((int)Screen.Size.X / 2, logoPosY, 6, 0, Image.titlelogo, 1);
+            if (state == State.Ready && flashTimer / 16 % 2 == 0)
+            {
+                DX.DrawRotaGraph((int)Screen.Size.X / 2, 600, 4, 0, Image.gamestart, 1);
+            }
+            if(state == State.Flash && flashTimer / 2 % 4 == 0)
+            {
+                DX.DrawRotaGraph((int)Screen.Size.X / 2, 600, 4, 0, Image.gamestart, 1);
+            }
         }
     }
 }
